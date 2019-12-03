4f355547aec58b5812cf52c80c1afb43488c2fd6
Katoinga <evaldiviaci@unsa.edu.pe>
2019-12-03 18:55:50 -0500

    v6 Logins

---
+++ app/src/main/java/com/example/kashop/Forgot.java
@@ -0,0 +1,14 @@
+package com.example.kashop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Forgot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot);
+    }
+}
