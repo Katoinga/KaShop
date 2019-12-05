41aa40173aba8c25342f0bebc373c326e67eb5fe
cgonzalesmo <58450963+cgonzalesmo@users.noreply.github.com>
2019-12-04 20:22:35 -0500

    Subiendo Item Details 2.

---
+++ app/src/main/java/com/example/kashop/Item_Details_2.java
@@ -0,0 +1,14 @@
+package com.example.kashop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Item_Details_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item__details_2);
+    }
+}
